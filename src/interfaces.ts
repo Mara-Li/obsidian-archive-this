ea85bb314455421561c7d1b3f927344d71c8db6e
Mara <mara-li@outlook.fr>
2026-03-04 21:52:46 +0100

    dev:starting

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -4,7 +4,15 @@ export interface ArchiveThisSettings {
 		inSource: boolean;
 		inArchive: boolean;
 	};
+	overridePaths: OverridePath[];
+	originalPathFrontmatterKey: string;
 }
+
+export type OverridePath = {
+	sourcePath: string;
+	archivePath: string;
+	regex: boolean;
+};
 
 export const DEFAULT_SETTINGS: ArchiveThisSettings = {
 	archiveFolder: "",
@@ -12,4 +20,6 @@ export const DEFAULT_SETTINGS: ArchiveThisSettings = {
 		inSource: false,
 		inArchive: false,
 	},
+	overridePaths: [],
+	originalPathFrontmatterKey: "original_path",
 };
